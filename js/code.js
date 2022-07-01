c55b707fa5e8ae7025055c89c08c74fb27ccfdf8
kassu11 <61124141+kassu11@users.noreply.github.com>
2022-07-01 22:04:44 +0300

    3 - Pikanapit

--- js/code.js
+++ js/code.js
@@ -13,11 +13,10 @@ if(location.search.length === 0) {
 
 	document.body.append(textarea, button);
 } else {
+	const loadedImages = [];
 	const images = location.search.substring(1).split("&");
 	if(location.hash.length === 0) location.hash = 1;
-	const image = document.createElement("img");
 	let index = parseInt(location.hash.substring(1));
-	image.src = images[index - 1];
 
 	const container = document.createElement("div");
 	const left = document.createElement("button");
@@ -25,18 +24,38 @@ if(location.search.length === 0) {
 	container.append(left, right);
 	left.textContent = "<";
 	right.textContent = ">";
+
+	window.addEventListener("keydown", e => {
+		const lastIndex = index;
+
+		if(e.key === "ArrowRight") {
+			if(++index > images.length) index = 1;
+		} else if(e.key === "ArrowLeft") {
+			if(--index < 1) index = images.length;
+		}
+
+		if(lastIndex !== index) updateImage();
+	})
+
 	left.addEventListener("click", e => {
 		if(--index < 1) index = images.length;
-		location.hash = index;
-		image.src = images[index - 1];
+		updateImage();
 	});
 
 	right.addEventListener("click", e => {
 		if(++index > images.length) index = 1;
-		location.hash = index;
-		image.src = images[index - 1];
+		updateImage();
 	});
 
-	document.body.append(image, container);
+	document.body.append(container);
+	updateImage();
 	
+	function updateImage() {
+		document.querySelector("img")?.remove();
+		const image = document.createElement("img");
+		container.before(image);
+		image.src = images[index - 1];
+		location.hash = index;
+		loadedImages[index - 1] = image;
+	}
 }
